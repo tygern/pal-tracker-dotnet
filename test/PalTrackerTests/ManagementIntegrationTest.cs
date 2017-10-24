488344af777d551a6103fcd72d91cf1a04330706
Alex Sartan <asartan@pivotal.io>
2017-10-24 14:15:42 -0600

    Add tests for health monitoring lab

---
+++ test/PalTrackerTests/ManagementIntegrationTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using static PalTrackerTests.DbTestSupport;
+
+namespace PalTrackerTests
+{
+    [Collection("Integration")]
+    public class ManagementIntegrationTest : IntegrationTest
+    {
+        protected override IDictionary<string, string> EnvironmentVariables => new Dictionary<string, string>
+        {
+            {"VCAP_SERVICES", TestDbVcapJson}
+        };
+
+        public ManagementIntegrationTest()
+        {
+            ExecuteSql("TRUNCATE TABLE time_entries");
+        }
+
+        [Fact]
+        public void HasHealth()
+        {
+            var response = TestHttpClient.GetAsync("/health").Result;
+            var responseBody = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("UP", responseBody["status"]);
+            Assert.Equal("UP", responseBody["diskSpace"]["status"]);
+            Assert.Equal("UP", responseBody["timeEntry"]["status"]);
+        }
+        
+        [Fact]
+        public void HasInfo()
+        {
+            var response = TestHttpClient.GetAsync("/info").Result;
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ test/PalTrackerTests/TimeEntryInfoContributorTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Moq;
+using PalTracker;
+using Steeltoe.Management.Endpoint.Info;
+using Xunit;
+
+namespace PalTrackerTests
+{
+    public class TimeEntryInfoContributorTest
+    {
+        private readonly Mock<IInfoBuilder> _builder;
+        private readonly Mock<IOperationCounter<TimeEntry>> _counter;
+        private readonly TimeEntryInfoContributor _contributor;
+        
+        public TimeEntryInfoContributorTest()
+        {
+            _builder = new Mock<IInfoBuilder>();
+            _counter = new Mock<IOperationCounter<TimeEntry>>();
+            _contributor = new TimeEntryInfoContributor(_counter.Object);
+        }
+        
+        [Fact]
+        public void Contribute()
+        {
+            var counts = new Dictionary<TrackedOperation, int>
+            {
+                {TrackedOperation.Create, 0},
+                {TrackedOperation.Read, 3},
+                {TrackedOperation.List, 1},
+                {TrackedOperation.Update, 7},
+                {TrackedOperation.Delete, 3}
+            };
+            
+            _builder.Setup(b => b.WithInfo(It.IsAny<string>(), It.IsAny<object>()));
+            _counter.Setup(c => c.GetCounts()).Returns(counts);
+            _counter.SetupGet(c => c.Name).Returns("TimeEntryOperations");
+
+            _contributor.Contribute(_builder.Object);
+
+            _builder.Verify(b => b.WithInfo("TimeEntryOperations", counts));
+        }
+    }
+}
